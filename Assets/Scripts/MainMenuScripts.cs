729fb4e2e92ae9d17ba74c45a2947004dac4a000
Anobile <anobile12@hotmail.com>
2016-03-17 07:26:37 -0400

    trying again level 1
    Not all of the push went

---
+++ Assets/Scripts/MainMenuScripts.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuScripts : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void newGame()
+    {
+        DontDestroyOnLoad(GameObject.FindGameObjectWithTag("EditorOnly"));
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void highScore()
+    {
+        DontDestroyOnLoad(GameObject.FindGameObjectWithTag("EditorOnly"));
+        SceneManager.LoadScene("High Score");
+    }
+}
